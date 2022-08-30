4a3cc66bca6d333349e60e2ade63e262773e6fdd
nokey93 <nnanhde@gmail.com>
2022-08-30 14:03:31 -0700

    New Images for Icon

--- Formelsammlung/ViewController.swift
+++ Formelsammlung/ViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
+    // MARK: - Test
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the
     }
-
-
 }
-
