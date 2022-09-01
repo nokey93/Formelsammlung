f7ab141c1e2be23f9be3ea06c64f43e98a82c2e7
nokey93 <nnanhde@gmail.com>
2022-09-01 04:18:22 -0700

    Add new Image

--- Formelsammlung/ViewController.swift
+++ Formelsammlung/ViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
-    // MARK: - Test
-
 class ViewController: UIViewController {
 
+    @IBOutlet weak var matheImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the
+        
+        matheImageView.image = #imageLiteral(resourceName: "Mittelsenkrechte")
     }
 }
+ 
